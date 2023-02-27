a7c43ea32d06ec65cd01258ae4be5af09ef2cbaa
treeCount <109946131+treeCount@users.noreply.github.com>
2023-02-27 17:30:55 +0900

    Maincopy (#1)
    * a파일 만듬
    
    * branch maincopyã수정

--- Main.kt
+++ Main.kt
@@ -1,5 +1,16 @@
 fun main(args: Array<String>) {
 
     //git 수정해서 다시 백업하기
-    println("Hello World! git Study!!")
+    println("Hello World! git Study!!") //git 로컬저장소에서 브랜치 만들고 conflict도 해보기
+
+    /*git
+    * git push
+    * -u는 한번 사용하면 그다음부터는 그 url등록
+    * git url 변수지정 하기
+    * git remote add origin 깃헙주소 main
+    *
+    * main은 git에 있는 main을 많이 사용하므로 브랜치 이름을 main이라 많이 사용
+    * origin 이라는 변수명은 개발관습으로 많이 활용한다함
+    *
+    * */
 }
